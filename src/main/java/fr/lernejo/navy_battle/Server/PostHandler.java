55c3ef98dfc5215700da9082bc16b771fb93c246
JoshhuaP <Joshua.pinneau@gmail.com>
2021-12-27 01:33:28 +0100

    Resolve stream closed problem

--- src/main/java/fr/lernejo/navy_battle/Server/PostHandler.java
+++ src/main/java/fr/lernejo/navy_battle/Server/PostHandler.java
@@ -1,23 +1,17 @@
 package fr.lernejo.navy_battle.Server;
-
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import fr.lernejo.navy_battle.Json.JsonParser;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.nio.charset.StandardCharsets;
-
 public class PostHandler implements HttpHandler {
     final String url;
-
-    public PostHandler(String url) {
-        this.url = url;
-    }
-
+    public PostHandler(String url) {this.url = url;}
+    public final void cleanup(OutputStream os) throws IOException {os.close();}
     public final void HttpClient() {
         HttpClient newClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -25,7 +19,6 @@ public class PostHandler implements HttpHandler {
             .POST(HttpRequest.BodyPublishers.ofString("Start"))
             .build();
     }
-
     public final String[] ArrayResponse(HttpExchange t) throws IOException {
         int status;
         String response;
@@ -40,13 +33,13 @@ public class PostHandler implements HttpHandler {
         arr[1] = response;
         return arr;
     }
-
     public final void handle(HttpExchange t) throws IOException {
         int status;
         String response;
         try {
-            status = Integer.parseInt(this.ArrayResponse(t)[0]);
-            response = this.ArrayResponse(t)[1];
+            String[] arr = this.ArrayResponse(t);
+            status = Integer.parseInt(arr[0]);
+            response = arr[1];
         }
         catch (Exception e) {
             status = 500;
@@ -56,6 +49,7 @@ public class PostHandler implements HttpHandler {
         t.sendResponseHeaders(status, response.getBytes(StandardCharsets.UTF_8).length);
         OutputStream os = t.getResponseBody();
         os.write(response.getBytes(StandardCharsets.UTF_8));
-        os.close();
+        os.flush();
+        this.cleanup(os);
     }
 }
